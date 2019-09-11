31ff39029c268344a85d97f3b5062b40decc0bf9
Stanislaw Hüll <Stanislaw.Huell@js-soft.com>
2019-09-11 08:50:07 +0200

    Wrap s1c2 in proper output

--- solutions/set1/s1c2.rs
+++ solutions/set1/s1c2.rs
@@ -4,7 +4,12 @@ fn main() {
     let input = "1c0111001f010100061a024b53535009181c";
     let xor_mask = Rawmem::from_hex("686974207468652062756c6c277320657965");
     let output = Rawmem::from_hex(&input).isolen_xor(&xor_mask);
-
     let target = Rawmem::from_hex("746865206b696420646f6e277420706c6179");
-    assert_eq!(target, output);
+
+    print!("Set 1 - Challenge 2: ");
+    if output == target {
+        println!("\x1b[1;32mCorrect!\x1b[0m");
+    } else {
+        println!("\x1b[1;31mIncorrect!\x1b[0m");
+    }
 }
